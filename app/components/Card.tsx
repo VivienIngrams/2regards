65149f7610c345abcfba353f63904a579b139909
VivienIngrams <vivingrams@gmail.com>
2024-01-09 14:58:38 +0000

    Adding divs with two images

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,22 +1,38 @@
-
 import React from "react";
 import { VisibilityContext } from "react-horizontal-scrolling-menu";
 import Image from "next/image";
 
-export default function Card({ img, title, id }: { img: string;
+export default function Card({
+  img,
+  title,
+  id,
+}: {
+  img: string;
   title: string;
-  id: string; }) {
+  id: string;
+}) {
   const visibility = React.useContext(VisibilityContext);
 
   const visible = visibility.isItemVisible(id);
 
   return (
     <div
-    key={id}
-    className='inline-block w-40 h-full m-2 select-none'
-           tabIndex={0}
-      >
-      <Image src={img} height={800} width={500} alt={title} />
+      key={id}
+      className="flex-col justify-between w-[400px] h-full m-2 select-none"
+      tabIndex={0}
+    >
+      <div className="h-1/2 w-auto relative">
+        <Image className="" src={img} height={300} width={300} alt={title} />
+      </div>
+      <div className="h-1/2 w-auto relative">
+        <Image
+          className=""
+          src={img}
+          height={300}
+          width={300}
+          alt={title}
+        />
+      </div>
     </div>
   );
 }
