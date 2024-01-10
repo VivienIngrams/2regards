56ee43316495e4ad97a101433246369cc499c6fe
VivienIngrams <vivingrams@gmail.com>
2024-01-10 13:37:04 +0000

    Border and input on Contact page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
   return (
     <footer className="fixed left-0 bottom-0 w-screen h-4 md:h-6 bg-stone-200">
       <div className="relative min-w-screen z-25 border-t-black border-[1px]">
-        <div className="absolute bottom-[2px] z-10 w-screen ml-6 bg-stone-200 h-3 md:h-6" />
+        <div className="absolute bottom-[2px] mx-6 bg-stone-200 h-3 md:h-6" />
         <div className="absolute text-md md:text-xl ml-6 md:ml-10 z-20 leading-loose -bottom-5  font-italiana gap-2 md:gap-10 flex">
           {menuItems.map((item) => (
             <Link key={item.text} href={item.href} className="bg-stone-200 p-1">
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,21 +2,62 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="mr-8 md:mr-12">
-      <div>Contact</div>
-      <div>
-        Mathilde Cudeville mathilde@2regards.com Tel: +33 6 79 76 14 88
-        www.2regards.com Paulo Bastos paulo@2regards.com Tel: +33 7 87 38 36 32
-        www.2regards.com Gallery atelier@2regards.com +351 933 119 390 Rua do
-        Duque da Terceira 358 4300-096 Porto Wednesday to Saturday / 2p.m -
-        7p.m. Link google
-        https://www.google.com/maps/place/2regards+atelier+photos+films/@41.1472147,-8.6012507,17z/data=!3m1!4b1!4m6!3m5!1s0xd2465a5d3873983:0xbfff3607a844ac77!8m2!3d41.1472147!4d-8.5986758!16s%2Fg%2F11p_1z0qdh?entry=ttu
-        Social Network https://www.instagram.com/2regards.photosfilms/
-        https://www.facebook.com/2regards.photosfilms
-        https://www.youtube.com/channel/UChl2gxRgunj929SvG_D8vyA
+    <div className="relative mx-20">
+       <div className="w-4 md:w-6 z-50 fixed h-screen right-0 top-0">
+        <div className="right-0 top-6 relative min-h-screen border-l-black border-[1px] bg-stone-200" />
+        
+      </div>
+      {/* Newsletter Subscription Form */}
+      <div className="relative flex flex-col items-end">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Your email here"
+          className="w-[400px] h-8 p-2 border border-black rounded my-2"
+        />
+        <button
+          type="submit"
+          className="bg-neutral-500 text-white h-8 px-4 border border-black rounded "
+        >
+          Subscribe to our newsletter
+        </button>
+      </div>
+
+      {/* Gallery Information */}
+      <div className="absolute left-14 top-8">
+        <div className="text-black text-5xl font-normal font-italiana">
+          gallery.
+        </div>
+        <div className="w-96 h-80 mt-8 text-neutral-500 text-lg font-light leading-relaxed">
+          <div>Rua do Duque da Terceira 358</div>
+          <div>4300-096 Porto</div>
+          <div>
+            Wednesday to Saturday <span>2p.m - 7p.m.</span>
+          </div>
+          <div>atelier@2regards.com</div>
+          <div>+351 933 119 390</div>
+        </div>
+      </div>
+
+      {/* Contact Information */}
+      <div className="absolute left-80 top-44">
+        <div className="text-black text-5xl font-normal font-italiana">
+          contact.
+        </div>
+        <div className="w-96 h-64 mt-4 text-right text-neutral-500 text-lg font-light leading-relaxed">
+          <div>Mathilde Cudeville copy</div>
+          <div>mathilde@2regards.com</div>
+          <div>Tel: +33 6 79 76 14 88</div>
+          <div>Paulo Bastos</div>
+          <div>paulo@2regards.com</div>
+          <div>Tel: +33 7 87 38 36 32</div>
+          <div>www.2regards.com</div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Contact;
+
