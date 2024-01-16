abbb6c7d475e0aa5c1eb911998e2432266e1fef7
VivienIngrams <vivingrams@gmail.com>
2024-01-16 17:32:27 +0000

    Responsive design Studio page

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -4,17 +4,23 @@ import Image from "next/image";
 
 const Studio = () => {
   return (
-    <div className="md:py-12 mr-16 text-neutral-500 text-sm text-justify md:text-left">
+    <div className="mr-8 md:py-12 md:mr-16  text-neutral-500 text-sm text-justify md:text-left">
+      {/* Border right */}
       <div className="w-4 md:w-6 z-50 fixed h-screen right-0 top-0">
         <div className="right-0 top-4 md:top-6 relative min-h-screen border-l-black border-[1px] bg-stone-100" />
       </div>
-      <div className=" grid md:grid-cols-2">
-        <div className="m-2 text-center text-black">
-          <p className=" text-[80px] font-normal font-italiana leading-[38px] tracking-tighter">
+
+      {/* Top section */}
+      <div className=" md:grid md:grid-cols-2">
+        {/* Title */}
+        <div className="m-16 md:m-2 text-center text-black">
+          <p className="text-3xl text-[80px] font-normal font-italiana leading-[38px] tracking-tighter">
             Studio
           </p>
         </div>
-        <div className=" -ml-4 mr-10">
+
+        {/* 1st, Top right section */}
+        <div className=" md:-ml-4 md:mr-10">
           <div className="flex border-b-2 border-black ">
             <div className="flex">
               <Image
@@ -38,9 +44,16 @@ const Studio = () => {
         </div>
       </div>
 
-      <div className="mt-6 grid md:grid-cols-2 leading-normal text-sm">
-        <div className="ml-14 mr-10">
-          <div className="flex border-b-2 border-black ">
+      {/*2nd, Left section */}
+      <div className="mt-6 md:grid md:grid-cols-2 leading-normal text-sm">
+        <div className="md:ml-14 md:mr-10">
+          
+          <div className="flex justify-end md:justify-start border-b-2 border-black ">
+          <div className="flex md:hidden items-center">
+              <h1 className="p-6 max-w-[160px] font-italiana text-4xl text-black leading-1">
+                Product Studios
+              </h1>
+            </div>
             <div className="flex">
               <Image
                 className=""
@@ -50,7 +63,7 @@ const Studio = () => {
                 alt="Paulo Bastos"
               />
             </div>
-            <div className="flex items-center">
+            <div className="hidden md:flex items-center">
               <h1 className="p-6 max-w-[160px] font-italiana text-4xl text-black leading-1">
                 Product Studios
               </h1>
@@ -67,9 +80,10 @@ const Studio = () => {
         <div className="ml-14 mr-10"></div>
       </div>
 
-      <div className="-mt-16 grid md:grid-cols-2 leading-normal text-sm">
-        <div className="ml-16 mr-10"></div>
-        <div className="ml-14 mr-10">
+      {/*3rd, Bottom right section */}
+      <div className="mt-6 pb-10 md:-mt-16 md:grid md:grid-cols-2 leading-normal text-sm">
+        <div className="md:ml-16 md:mr-10"></div>
+        <div className="md:ml-14 md:mr-10">
           <div className="flex border-b-2 border-black ">
             <div className="flex">
               <Image
