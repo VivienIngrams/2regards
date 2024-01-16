4f49e446413f148de6c5d5ed4dbd2c50ce354b77
VivienIngrams <vivingrams@gmail.com>
2024-01-16 18:28:02 +0000

    Design for Product page

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 import Image from "next/image";
+import Link from "next/link";
 
 const Product = () => {
   return (
-    <div className="md:py-12 mr-16 text-neutral-500 text-sm ">
-
+    <div className="relative md:mr-16 text-neutral-500 text-sm h-full">
       {/* Nav buttons */}
       <div className="w-24 fixed right-8 top-16">
-        <div className="">Back</div>
-        <div className="">--</div>
+        <Link href="/">
+          <div className="">Back</div>
+        </Link>
+        <div>
+          <Link href=""></Link>
+          <div className="">--</div>
+          <Link href=""></Link>
+        </div>
       </div>
 
       {/* Right border */}
@@ -17,40 +23,41 @@ const Product = () => {
         <div className="right-0 top-4 md:top-6 relative min-h-screen border-l-black border-[1px] bg-stone-100" />
       </div>
 
-      {/* Header */}
-      <div className="m-2 flex items-center justify-center text-center text-neutral-400">
+      {/* Title */}
+      <div className="absolute p-12 w-full flex items-center justify-center text-center text-neutral-400">
         <div className="max-w-[160px]">
-        <p className=" text-[80px] font-normal font-italiana leading-[38px] tracking-tighter">
-          Title of product
-        </p></div>
+          <p className=" text-[80px] font-normal font-italiana leading-[38px] tracking-tighter">
+            Title of product
+          </p>
+        </div>
       </div>
-     
-      <div className=" grid md:grid-cols-2">
+
+      <div className="md:grid md:grid-cols-2 min-h-full max-h-[90vh]">
         {/* Left side of page */}
-        <div>
-          <div className="flex">
+        <div className="md:grid md:grid-rows-3 md:w-full max-h-[90vh]">
+          <div className="z-1 pl-12 flex relative h-full ">
             <Image
-              className=""
+              className="object-fill overflow-visible absolute"
+              src="/images/lights.png"
+              height={400}
+              width={400}
+              alt="Paulo Bastos"
+            />
+          </div>
+          <div className="z-2 flex relative h-full justify-end">
+            <Image
+              className=" absolute"
               src="/images/Paulo2.jpeg"
-              width={100}
+              width={200}
               height={200}
               alt="Paulo Bastos"
             />
           </div>
-          <div className="flex">
+          <div className="z-3 pl-28 flex justify-center h-full relative">
             <Image
-              className=""
+              className=" absolute bottom-0"
               src="/images/Paulo2.jpeg"
-              width={100}
-              height={200}
-              alt="Paulo Bastos"
-            />
-          </div>
-          <div className="flex">
-            <Image
-              className=""
-              src="/images/Paulo2.jpeg"
-              width={100}
+              width={200}
               height={200}
               alt="Paulo Bastos"
             />
@@ -58,7 +65,7 @@ const Product = () => {
         </div>
 
         {/* Right side of page */}
-        <div className="flex items-center justify-center ml-24">
+        <div className="flex items-center justify-center mt-32 ml-32">
           <div className="mx-auto max-w-sm">
             <h1 className="font-italiana text-4xl text-black leading-1">
               Workshops
