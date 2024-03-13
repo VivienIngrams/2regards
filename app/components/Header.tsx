e6044ff0132f6c2cd5c2b37d7d7e0039ed2f69fb
VivienIngrams <vivingrams@gmail.com>
2024-03-13 15:52:45 +0000

    Dimensions Logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 // Header.js
 const Header = () => {
@@ -7,8 +8,13 @@ const Header = () => {
       <div className="z-50 fixed w-full top-0 flex justify-center border-b-[1px] border-black h-4 md:h-6 bg-stone-200" />
       <div className=" w-4 md:w-6 z-999 fixed h-screen left-0 top-4">
         <div className="left-0 top-0 relative min-h-screen border-r-black border-[1px] bg-stone-200" />
-        <div className="-left-1 md:left-1 p-1 absolute font-italiana top-[50%] bg-stone-200 origin-top-left -rotate-90 text-black text-xl md:text-2xl">
-          2regards
+        <div className="-left-1 md:left-1 absolute  top-[55%] origin-top-left -rotate-90 h-16 w-32 p-1 bg-stone-200">
+          <Image
+          src="/2regards.png"
+          alt="2regards"
+          width={200}
+          height={100}
+          />
         </div>
       </div>
     </header>
