c9fab5e35a3d7e0a3d80c2285d19987bab3db713
VivienIngrams <vivingrams@gmail.com>
2024-03-19 15:45:04 +0000

    New logo for About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,59 +21,68 @@ const About = () => {
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
-            className="my-[2vh] ml-[25%] xs:ml-[45%] lg:mx-auto text-right lg:text-left lg:max-w-[30vw]"
+            className="my-[2vh] ml-[25%] xs:ml-[45%] lg:mx-auto text-right leading-[1.2em] lg:text-left lg:max-w-[30vw]"
           >
             <p>
-              <span className="text-3xl lg:leading-[22px]">D</span>edicated to
-              creation and visual arts, 2regards Atelier was founded in Porto
-              by the photographer Mathilde Cudeville and videographer Paulo
-              Bastos. With a focus on advertising, they transform
-              products into captivating visual stories that encapsulate the
-              essence of a brand through photography, video and graphic design.
+              <span className="text-3xl leading-none lg:leading-[22px]">D</span>edicated to
+              creation and visual arts, 2regards Atelier was founded in Porto by
+              the photographer Mathilde Cudeville and videographer Paulo Bastos.
+              With a focus on advertising, they transform products into
+              captivating visual stories that encapsulate the essence of a brand
+              through photography, video and graphic design.
             </p>
           </motion.div>
 
           {/* Title desktop */}
-          <div className="hidden lg:flex justify-center items-center m-4 text-center text-black">
-            <div>
-              <h1 className=" hidden lg:block text-6xl lg:text-[80px] font-normal font-italiana uppercase leading-[30px] lg:leading-[38px] tracking-tighter">
-                2 regards{" "}
+          <div className="hidden lg:flex justify-center items-center m-4 ">
+            {/* <div>
+              <h1 className=" hidden lg:block text-6xl lg:text-[80px] font-normal font-italiana leading-[30px] lg:leading-[42px] tracking-tighter">
+                2regards{" "}
               </h1>
-              <h1 className="hidden lg:block text-6xl lg:text-[80px] font-normal font-italiana leading-[30px] lg:leading-[38px] tracking-tighter">
+              <h1 className="hidden lg:block text-6xl lg:text-[80px] font-normal font-italiana leading-[30px] lg:leading-[42px] tracking-tighter">
                 Atelier
               </h1>
-            </div>
+            </div> */}
+            <Image
+              src="/2regards-black.png"
+              alt="2regards"
+              width={300}
+              height={200} />
           </div>
 
           {/* Title mobile */}
         </div>
-        <div>
-          <div className="lg:hidden m-4 text-center text-black">
-            <h1 className="lg:hidden text-4xl xs:text-6xl  font-normal font-italiana uppercase leading-[20px] xs:leading-[30px] lg:leading-[38px] tracking-tighter">
-              2 regards{" "}
+        <div className="lg:hidden flex justify-center  m-4">
+        <Image
+              src="/2regards-black.png"
+              alt="2regards"
+              width={200}
+              height={100} />
+          {/* <div className="lg:hidden m-4 text-center text-black">
+            <h1 className="lg:hidden text-4xl xs:text-6xl  font-normal font-italiana leading-[20px] xs:leading-[30px]  tracking-tighter">
+              2regards{" "}
             </h1>
-            <h1 className="lg:hidden text-5xl xs:text-6xl  font-normal font-italiana leading-[20px] xs:leading-[30px] lg:leading-[38px] tracking-tighter">
+            <h1 className="lg:hidden text-5xl xs:text-6xl  font-normal font-italiana leading-[20px] xs:leading-[30px] tracking-tighter">
               Atelier
             </h1>
-          </div>
+          </div> */}
         </div>
         <div className="flex flex-col justify-center items-center">
           <motion.div
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
-            className="mt-[2vh] mr-[25%] xs:mr-[45%] lg:m-auto  text-left lg:max-w-[30vw]"
+            className="mt-[2vh] mr-[25%] xs:mr-[45%] lg:m-auto leading-[1.2em] text-left lg:max-w-[30vw]"
           >
             <p>
-              <span className="text-3xl tracking-tighter lg:leading-[22px]">
+              <span className="text-3xl tracking-tighter leading-none lg:leading-[22px]">
                 T
               </span>
-              <span className="tracking-tighter">h</span>e Atelier features an
-              art gallery welcoming local and international artists
-              throughout the year. The space includes a commercial photography
-              studio, an analog laboratory and post-production offices, where
-              training sessions for analogue capture and development are
-              facilitated.
+              <span className="tracking-tighter leading-none">h</span>e Atelier features an
+              art gallery welcoming local and international artists throughout
+              the year. The space includes a commercial photography studio, an
+              analog laboratory and post-production offices, where training
+              sessions for analogue capture and development are facilitated.
             </p>
           </motion.div>
           <div className="lg:hidden flex justify-center pb-4">
@@ -96,7 +105,7 @@ const About = () => {
           <div className="flex border-b-2 border-black ">
             <div className="flex min-w-[85px]">
               <Image
-              className="border-[1px] border-black"
+                className="border-[1px] border-black"
                 src="/images/Mathilde.jpeg"
                 width={140}
                 height={200}
@@ -112,23 +121,24 @@ const About = () => {
           <p className="mt-2 pb-2 ">
             Mathilde Cudeville&rsquo;s artistic practice revolves around the
             photographic medium and silver printing, with her works oscillating
-            between ambient photography and plastic photography. 
+            between ambient photography and plastic photography.
           </p>
           <p className="pb-2">
             Mathilde absorbs the world around her and reflects it back with a
             highly personal poetic touch. She dissolves into the surroundings
             before revealing the most unusual elements, which are often the most
-            expressive.           </p>
+            expressive.{" "}
+          </p>
           <p className="pb-2 ">
-            Initially delving into insularity and
-            isolated territories, notably through a documentary on the
-            Saint-Pierre-et-Miquelon archipelago (&quot;Les onze mille
-            vierges&quot;), she then engaged in a correspondence book, playing
-            the exquisite corpse game with a writer in Paris.             Subsequently, she embarked on a series of self-portraits
+            Initially delving into insularity and isolated territories, notably
+            through a documentary on the Saint-Pierre-et-Miquelon archipelago
+            (&quot;Les onze mille vierges&quot;), she then engaged in a
+            correspondence book, playing the exquisite corpse game with a writer
+            in Paris. Subsequently, she embarked on a series of self-portraits
             (&quot;Rêverie Odisseia&quot;), baring herself both literally and
-            figuratively. &quot;En suspension (S)&quot; follows a
-            similarly liberated dual approach, using otherness as a vehicle,
-            without artifice or mask.{" "}
+            figuratively. &quot;En suspension (S)&quot; follows a similarly
+            liberated dual approach, using otherness as a vehicle, without
+            artifice or mask.{" "}
           </p>
           <p> MICHEL LE ROUX, Poet and friend</p>
         </div>
@@ -161,16 +171,20 @@ const About = () => {
             company 2regards, which he co-founded with his partner Mathilde
             Cudeville.
           </p>
-          <Link
-              href="https://www.youtube.com/shorts/_8F3bTh_rkg"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="p-1 bg-white font-italiana text-xl text-black h-8 px-4 border border-black rounded "
-            >
-              Watch video
-            </Link>
         </div>
       </motion.div>
+        <div
+          className="flex justify-center pb-12"
+        >
+          <Link
+            href="https://www.youtube.com/shorts/_8F3bTh_rkg"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="p-1 bg-white font-italiana text-2xl text-black h-10 px-4 border border-black rounded "
+          >
+            2regards Atelier
+          </Link>
+        </div>
     </div>
   );
 };
