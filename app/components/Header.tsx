76d9833815a1ddf631c9d7d5c1be85e6c848ca2b
VivienIngrams <vivingrams@gmail.com>
2024-03-20 11:25:14 +0000

    Reverting changes to products and header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,12 +4,17 @@ import Image from "next/image";
 // Header.js
 const Header = () => {
   return (
-    <header >
-     <div className="flex justify-center border-b-[1px] border-black h-4 md:h-6"/>
-      <div className="w-4 md:w-6 z-999 fixed h-screen left-0 top-0">
-        <div className="left-0 top-0 relative min-h-screen border-r-black border-[1px] bg-stone-100" />
-        <div className="-left-1 md:left-1 p-1 absolute font-italiana top-[50%] bg-stone-100 origin-top-left -rotate-90 text-black text-xl md:text-2xl">
-          2regards
+    <header>
+      <div className="z-50 fixed w-full top-0 flex justify-center border-b-[1px] border-black h-4 md:h-6 bg-stone-200" />
+      <div className=" w-4 md:w-6 z-999 fixed h-screen left-0 top-4">
+        <div className="left-0 top-0 relative min-h-screen border-r-black border-[1px] bg-stone-200" />
+        <div className="-left-1 md:left-1 absolute  top-[55%] origin-top-left -rotate-90 h-8 md:h-12 w-28 p-1 bg-stone-200">
+          <Image
+          src="/2regards.png"
+          alt="2regards"
+          width={200}
+          height={100}
+          />
         </div>
       </div>
     </header>
