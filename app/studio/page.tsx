31ea6113ea303ab3987dbbb65fefe07de061b6bc
VivienIngrams <vivingrams@gmail.com>
2024-03-25 15:07:47 +0000

    Testing layouts for Studio page

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -14,7 +14,7 @@ const Studio = () => {
       </div>
 
       {/* Title */}
-      <div className="pt-4 lg:pt-12 pr-5 xs:pr-0 z-25 flex absolute max-w-[80vw] md:max-w-[90vw] w-full  items-center justify-center  text-black">
+      <div className="pt-4 lg:pt-6 pr-5 xs:pr-0 z-25 flex absolute max-w-[80vw] md:max-w-[90vw] w-full  items-center justify-center  text-black">
         <div className="">
           <h1 className="text-3xl lg:text-6xl xl:text-[80px] font-normal font-italiana text-center lg:leading-[26px] xl:leading-[38px] tracking-tighter">
             Studio
@@ -29,10 +29,10 @@ const Studio = () => {
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5 }}
-          className="flex flex-col lg:grid lg:grid-cols-2 "
+          className="flex flex-col lg:grid lg:grid-cols-5 "
         >
-          <div className=" relative flex flex-col md:flex-row -my-8">
-            <div className="relative flex w-full h-[25vh] xs:h-[30vh] md:w-[300px] md:h-[35vh] lg:w-[370px]  xl:w-[400px]  2xl:w-[340px] 2xl:h-[210px] 3xl:w-[380px] 3xl:h-[250px]">
+          <div className=" relative col-span-2 flex flex-col md:flex-row -my-8">
+            <div className="relative flex w-full h-[25vh] xs:h-[30vh] md:full md:h-[35vh] lg:w-[85%]  xl:h-full   3xl:w-[60%] ">
               <Image
                 className="border-[1px] border-black shadow-md shadow-neutral-500"
                 src="/images/studio/AnalogLab.jpg"
@@ -40,19 +40,20 @@ const Studio = () => {
                 alt="Analog laboratory"
               />
             </div>
-            <div className="flex justify-center items-center">
-              <h1 className="py-2 xs:py-6 md:pl-12 font-italiana text-xl xs:text-2xl sm:text-3xl text-black leading-1">
+          </div>
+          
+          <div className="px-24 col-span-3 lg:flex flex-col justify-center items-start">
+            <div className="flex justify-center items-center ">
+              <h1 className="font-italiana text-xl xs:text-2xl sm:text-3xl text-black leading-1">
                 Analog Laboratory
               </h1>
             </div>
-          </div>
-          <div className="lg:flex flex-col justify-center items-end">
-            <p className="mb-2 md:my-4">
+            <p className="">
               Also 2regards atelier features an analog photography lab for
               development and printing. It is also possible to rent this space
               by the hour or day. Click here to discover our rates.
             </p>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-end items-end">
               <Link
                 href="https://www.youtube.com/shorts/U5_XJuAa-tk"
                 target="_blank"
@@ -70,10 +71,17 @@ const Studio = () => {
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5 }}
-          className="flex flex-col lg:grid lg:grid-cols-2 "
+          className="flex flex-col lg:grid lg:grid-cols-5 "
         >
-          <div className="lg:flex flex-col justify-center items-end">
-            <p className="mt-2">
+          
+
+          <div className="px-24 col-span-3 lg:flex flex-col justify-center items-start">
+            <div className="flex justify-center items-center ">
+              <h1 className="font-italiana text-xl xs:text-2xl sm:text-3xl text-black leading-1">
+                Product Studios
+              </h1>
+            </div>
+            <p className="">
               2regards atelier offers professional quality photographic services
               to meet all your needs. We capture your studio shots for
               e-commerce, ranging from simple cutout packshots on a white
@@ -82,13 +90,9 @@ const Studio = () => {
               studio in Porto.
             </p>
           </div>
-          <div className="flex justify-end lg:justify-start ">
-            <div className="flex items-center">
-              <h1 className="p-1 xs:p-6 max-w-[160px] font-italiana text-xl xs:text-2xl sm:text-3xl text-black leading-1">
-                Product Studios
-              </h1>
-            </div>
-            <div className="relative flex w-full h-[25vh] xs:h-[30vh] md:w-[300px] md:h-[35vh] lg:w-[370px]  xl:w-[400px]  2xl:w-[340px] 2xl:h-[210px] 3xl:w-[380px] 3xl:h-[250px]">
+          
+          <div className=" relative col-span-2 flex flex-col md:flex-row -my-8">
+            <div className="relative flex w-full h-[25vh] xs:h-[30vh] md:full md:h-[35vh] lg:w-[85%]  xl:h-full   3xl:w-[60%] ">
               <Image
                 className="border-[1px] border-black shadow-md shadow-neutral-500"
                 src="/images/studio/ProductStudio.jpg"
@@ -105,10 +109,10 @@ const Studio = () => {
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5 }}
-          className="flex flex-col lg:grid lg:grid-cols-2 "
+          className="flex flex-col lg:grid lg:grid-cols-5 "
         >
-          <div className=" relative flex flex-col md:flex-row -my-8">
-            <div className="relative flex w-full h-[25vh] xs:h-[30vh] md:w-[300px] md:h-[35vh] lg:w-[370px]  xl:w-[400px]  2xl:w-[340px] 2xl:h-[210px] 3xl:w-[380px] 3xl:h-[250px]">
+          <div className=" relative col-span-2 flex flex-col md:flex-row -my-8">
+            <div className="relative flex w-full h-[25vh] xs:h-[30vh] md:full md:h-[35vh] lg:w-[85%]  xl:h-full   3xl:w-[60%] ">
               <Image
                 className="border-[1px] border-black shadow-md shadow-neutral-500"
                 src="/images/studio/Workshops.jpg"
@@ -116,14 +120,14 @@ const Studio = () => {
                 alt="Paulo Bastos"
               />
             </div>
-            <div className="flex justify-center items-center">
-              <h1 className="py-2 xs:py-6 md:pl-12 font-italiana text-xl xs:text-2xl sm:text-3xl text-black leading-1">
+          </div>
+          <div className="px-24 col-span-3 lg:flex flex-col justify-center items-start">
+            <div className="flex justify-center items-center ">
+              <h1 className="font-italiana text-xl xs:text-2xl sm:text-3xl text-black leading-1">
                 Workshops
               </h1>
             </div>
-          </div>
-          <div className="lg:flex flex-col justify-center items-end">
-            <p className="mb-2 md:my-4">
+            <p className="">
               Throughout the year, we organize workshops for participants to
               explore or deepen their knowledge in analog photography.
             </p>
